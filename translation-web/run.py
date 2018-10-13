d3315939abf8e1d9b1573be873fa932c31229cda
Pinkagit <pinka.wuquan@gmail.com>
2018-10-13 11:01:33 +0800

    udpate run.py

--- translation-web/run.py
+++ translation-web/run.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-#nohup ./node_modules/.bin/runkoa bin/porsche > out.file 2>&1 &
-
 import os
 import sys
+import time
+import getpass
 
-def do_action(action):
-    if action == 'build':
-        build()
-    pass
+process = 'translation-web'
+
+def start():
+    os.system("pm2 start app.js")
+
+def stop():
+    os.system("pm2 stop app")
+
+
+def restart():
+    os.system("pm2 restart app")
+
+def state():
+    os.system('pm2 show app')
 
 def build():
     print(">> --- before build, make sure install necessary packages")
@@ -18,14 +28,30 @@ def build():
     os.chdir('static/vue-translation/')
     os.system("npm run build")
 
+
+def do_action(action):
+    if action == 'start':
+        start()
+    elif action == 'stop':
+        stop()
+    elif action == 'restart':
+        stop()
+        start()
+    elif action == 'build':
+        build()
+    else:
+        state()
+    pass
+
+
 if __name__ == "__main__":
     opt = sys.argv
-    print(">>>> -------------------------------------------")
+    print ">>>> -------------------------------------------"
     if len(opt) == 1:
-        print("Usage: ")
-        print("python run.py [cmd:build]")
+        print "Usage: "
+        print "python run.py [cmd:start/stop/restart/build]"
         exit()
 
     action = opt[1]
     do_action(action)
-    print("<<<< -------------------------------------------")
+    print "<<<< -------------------------------------------"
